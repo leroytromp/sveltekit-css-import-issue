e72da2c837f2ac88f6000d665d77ae72175a52bb
Leroy Tromp <l.a.j.tromp@gmail.com>
2021-06-23 22:02:43 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '@patternfly/patternfly/patternfly.min.css';
+</script>
+
+<h1 class="pf-c-title pf-m-4xl">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="pf-c-button pf-m-primary" type="button">Primary</button>
+
+<button class="pf-c-button pf-m-control" type="button" aria-label="Copy input">
+	<i class="fas fa-copy" aria-hidden="true" />
+</button>
